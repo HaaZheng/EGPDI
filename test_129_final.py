c6f38f0321e6d9ca010b2913feae0e307a3ca053
HaaZheng <163391767+HaaZheng@users.noreply.github.com>
2024-04-01 11:01:49 +0800

    Update test_129_final.py

--- test_129_final.py
+++ test_129_final.py
@@ -30,26 +30,21 @@ parser.add_argument("--model_path", type=str, default='/home/lichangyong/Documen
 args = parser.parse_args()
 
 
-
 features = []
 labels = []
 
 class CustomDataset(Dataset):
-
     def __init__(self, features, labels):
         self.features = features
         self.labels = labels
-
     def __len__(self):
         return len(self.features)
-
     def __getitem__(self, index):
         return self.features[index], self.labels[index]
 
 
 # predicted pdb
 root_dir = args.dataset_path
-
 train_path= root_dir + 'DNA-573_Train.txt'
 test_path= root_dir + 'DNA-129_Test.txt'
 all_702_path = root_dir +  'DNA-702.txt'
@@ -77,13 +72,10 @@ with open(test_path, 'r') as f:
 X,y = create_dataset(query_ids,train_path, test_path,all_702_path, pkl_path,esm2_33_path,esm2_5120_path,ProtTrans_path,mas_path,residue=True,one_hot=True,esm2_33=True,esm_5120=True,prottrans=True,msa=True)
 distance_matrixs=create_dis_matrix(dis_path,query_ids)
 
-
 X_test = X[573:]
 y_test = y[573:]
 
-
 NUMBER_EPOCHS = 35
-
 dr=0.3
 lr=0.0001
 nlayers=4
@@ -91,10 +83,7 @@ lamda=1.1
 alpha=0.1
 atten_time=8
 
-
-
 IDs = query_ids[573:]
-
 sequences = []
 labels = []
 with open(all_702_path,'r') as f:
@@ -106,8 +95,6 @@ with open(all_702_path,'r') as f:
         labels.append(label)
 
 sequences = sequences[573:]
-
-
 labels = y_test
 features = X_test
 
@@ -120,21 +107,16 @@ for adj in adjs:
     G = dgl.graph((edge_index[0], edge_index[1])).to(device)
     graphs.append(G)
 
-
 # edge features
 save_edgefeats_path = args.edgefeats_path
 with open(save_edgefeats_path, 'rb') as f:
     efeats = pickle.load(f)
 
-
 test_dic = {"ID": IDs, "sequence": sequences, "label": labels,'features':features,'coors':coors,'adj':adjs,'graph':graphs,'efeats':efeats}
 dataframe = pd.DataFrame(test_dic)
 
-
 class dataSet(data.Dataset):
-
     def __init__(self,dataframe,adjs):
-
         self.names = dataframe['ID'].values
         self.sequences = dataframe['sequence'].values
         self.labels = dataframe['label'].values
@@ -145,7 +127,6 @@ class dataSet(data.Dataset):
         self.adj = dataframe['adj'].values
 
     def __getitem__(self,index):
-
         sequence_name = self.names[index]
         sequence = self.sequences[index]
         label = self.labels[index]
@@ -161,23 +142,15 @@ class dataSet(data.Dataset):
 
         return sequence_name,sequence,label,node_features,graph,efeat,adj,coor
 
-
     def __len__(self):
-
         return len(self.labels)
 
-
 def graph_collate(samples):
-
     _,_,label_batch, node_features_batch, graph_batch,efeat_batch,adj_batch,coors_batch = map(list, zip(*samples))
-
     graph_batch = dgl.batch(graph_batch)
-
     return label_batch, node_features_batch, graph_batch,efeat_batch,adj_batch,coors_batch
 
-
 def evaluate(model,data_loader):
-
     model.eval()
 
     epoch_loss = 0.0
@@ -185,11 +158,9 @@ def evaluate(model,data_loader):
     valid_pred = []
     valid_true = []
 
-
     for label_batch, node_features_batch, graph_batch,efeat_batch,adj_batch,coors_batch in data_loader:
 
         with torch.no_grad():
-
             node_features_batch = torch.tensor(node_features_batch)
             coors_batch = torch.tensor(coors_batch)
             adj_batch = adj_batch[0]
@@ -237,19 +208,15 @@ def evaluate(model,data_loader):
 
 
 def analysis(y_true,y_pred,best_threshold = None):
-
     if best_threshold == None:
         best_mcc = 0
         best_threshold = 0
 
         for j in range(0, 100):
-
             threshold = j / 100000
-
             binary_pred = [1 if pred >= threshold else 0 for pred in y_pred]
             binary_true = y_true
             mcc = matthews_corrcoef(binary_true, binary_pred)
-
             if mcc > best_mcc:
                 best_mcc = mcc
                 best_threshold = threshold
@@ -281,15 +248,12 @@ def analysis(y_true,y_pred,best_threshold = None):
 def test_129(Model_Path):
 
     model = MainModel(dr, lr, nlayers, lamda, alpha, atten_time, nfeats=71 + 20 + 256 + 1024 + 5120 + 33).to(device)
-
     model.load_state_dict(torch.load(Model_Path,map_location=device))
 
     test_dataSet = dataSet(dataframe=dataframe, adjs=adjs)
-
     test_loader = torch.utils.data.DataLoader(test_dataSet, batch_size=1, shuffle=False, collate_fn=graph_collate)
 
     _, test_true, test_pred = evaluate(model, test_loader)
-
     test_results = analysis(test_true, test_pred)
 
     return test_results
